29032cf6b44bace4b38d153c3c55337a950466a9
YvetteMiao <mywyvette220@gamil.com>
2020-06-16 23:39:47 +0200

    add question page

--- Navigator/app/src/main/java/com/example/navigater/model/Question.java
+++ Navigator/app/src/main/java/com/example/navigater/model/Question.java
@@ -2,30 +2,51 @@ package com.example.navigater.model;
 
 public class Question {
     int id;
-    String question;
-    String answer;
+    String question_1;
+    String question_2;
+    String question_3;
+
+    public Question() {
+    }
+
+    public Question(int id, String question_1, String question_2, String question_3) {
+        this.id = id;
+        this.question_1 = question_1;
+        this.question_2 = question_2;
+        this.question_3 = question_3;
+    }
 
     public int getId() {
         return id;
+    }
+
+    public String getQuestion_1() {
+        return question_1;
+    }
+
+    public void setQuestion_1(String question_1) {
+        this.question_1 = question_1;
+    }
+
+    public String getQuestion_2() {
+        return question_2;
+    }
+
+    public void setQuestion_2(String question_2) {
+        this.question_2 = question_2;
+    }
+
+    public String getQuestion_3() {
+        return question_3;
+    }
+
+    public void setQuestion_3(String question_3) {
+        this.question_3 = question_3;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getQuestion() {
-        return question;
-    }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
 }

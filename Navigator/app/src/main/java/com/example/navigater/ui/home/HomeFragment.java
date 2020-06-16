3a85f1d367bde70bb59c69cc6fe68c33e45b9841
CharlotteYU <wojizhilian@126.com>
2020-06-16 17:49:57 +0200

    get age

--- Navigator/app/src/main/java/com/example/navigater/ui/home/HomeFragment.java
+++ Navigator/app/src/main/java/com/example/navigater/ui/home/HomeFragment.java
@@ -61,10 +61,11 @@ public class HomeFragment extends Fragment {
             assert activity != null;
             String str = activity.getMyDb().getPatient(1).getFirstname();
             int a = activity.getMyDb().getPatient(1).getAge();
+            String age_1 = Integer.toString(a);
             TextView name = view.findViewById(R.id.patient_name);
             TextView age = view.findViewById(R.id.patient_age);
             name.setText(str);
-            age.setText(a);
+            age.setText(age_1);
         }
     }
 
